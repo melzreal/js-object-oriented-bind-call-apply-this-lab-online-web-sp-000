0ec52a617b5e3f6cf722da84bee50ea3ea9cc437
Melissa Real <melissa.real@hotmail.com>
2019-04-13 14:06:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn){
+  
+}
+
+function setThisWithCall(fn, thisValue, arg){
+  
+}
+
+function setThisWithApply(fn, thisValue, args){
+  
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  
+}
